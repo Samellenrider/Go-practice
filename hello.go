17bd39bfcbddc3be58d5c256094df74adcd988d1
Sam Ellenrieder <sam.ell@web.de>
2018-01-15 14:26:00 +0000

    first commit

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world");
+
+	var age = 20;
+
+	var faveNum float64 = 1.02385;
+
+	fmt.Println(age);
+	fmt.Println(faveNum);
+	
+}
