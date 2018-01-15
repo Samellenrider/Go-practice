7aef1345e1a8d8976c47c2d6b7635fe7f97776a9
Sam Ellenrieder <sam.ell@web.de>
2018-01-15 15:07:11 +0000

    basic knowledge

--- hello.go
+++ hello.go
@@ -11,5 +11,41 @@ func main() {
 
 	fmt.Println(age);
 	fmt.Println(faveNum);
-	
+
+	var myName string = "Sam";
+
+	fmt.Println(len(myName));
+
+	var yourAge int = 15;
+
+	if yourAge < 18 {
+		fmt.Println("You Can't vote");
+	} else if yourAge >= 18 {
+		fmt.Println("You Can Vote");
+	}
+
+	switch yourAge{
+		case 16 : fmt.Println("No vote for you");
+	    case 18 : fmt.Println("Go vote");
+	    default : fmt.Println("Free spirit bruddah");
+	}
+
+	var faveNums[5] int
+
+	faveNums[0] = 20
+	faveNums[1] = 17
+	faveNums[2] = 24
+	faveNums[3] = 43
+	faveNums[4] = 61
+
+	fmt.Println(faveNums);
+
+	for i, value := range faveNums {
+		fmt.Println(value, i)
+	}
+
+	for _, value := range faveNums {
+		fmt.Println(value)
+	}
+
 }
